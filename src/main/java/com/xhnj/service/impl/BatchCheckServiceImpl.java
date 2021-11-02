65969a5f31ca50a1a07e25152864e3f2919399d4
601881076 <601881076@qq.com>
2021-11-02 23:35:37 +0800

    授权取消审批页面-批准、拒绝操作

--- src/main/java/com/xhnj/service/impl/BatchCheckServiceImpl.java
+++ src/main/java/com/xhnj/service/impl/BatchCheckServiceImpl.java
@@ -6,6 +6,8 @@ import com.alibaba.excel.support.ExcelTypeEnum;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.xhnj.enums.BatchStatusEnum;
+import com.xhnj.enums.CheckResultEnum;
+import com.xhnj.enums.CheckStatusEnum;
 import com.xhnj.enums.SourceTypeEnum;
 import com.xhnj.mapper.TBatchCheckMapper;
 import com.xhnj.mapper.TDismissBatchQueryMapper;
@@ -39,6 +41,7 @@ public class BatchCheckServiceImpl implements BatchCheckService {
 
         list.stream().forEach(item -> {
             log.info("sourceType = " + item.getSourceType());
+            // 资源类型
             switch (item.getSourceType()) {
                 case "0" :
                     item.setSourceType(SourceTypeEnum.sourceTypeDD.desc());
@@ -53,6 +56,7 @@ public class BatchCheckServiceImpl implements BatchCheckService {
                     break;
             }
 
+            // 状态
             switch (item.getStatus()) {
                 case "-1" :
                     item.setStatus(BatchStatusEnum.STATUS_HOLD.desc());
@@ -72,7 +76,39 @@ public class BatchCheckServiceImpl implements BatchCheckService {
                 default:
                     break;
             }
-            log.info(item.toString());
+
+            // 审核结果
+            switch (item.getCheckResult()) {
+                case "0" :
+                    item.setCheckResult(CheckResultEnum.CHECK_RESULT_WATI.desc());
+                    break;
+                case "1" :
+                    item.setCheckResult(CheckResultEnum.CHECK_RESULT_PASS.desc());
+                    break;
+                case "2" :
+                    item.setCheckResult(CheckResultEnum.CHECK_RESULT_REFUSE.desc());
+                    break;
+                default:
+                    break;
+            }
+
+            // 审核状态
+            switch (item.getChkStatus()) {
+                case "0" :
+                    item.setChkStatus(CheckStatusEnum.CHECK_STATUS_WAIT.desc());
+                    break;
+                case "1" :
+                    item.setChkStatus(CheckStatusEnum.CHECK_STATUS_COMMIT.desc());
+                    break;
+                case "2" :
+                    item.setChkStatus(CheckStatusEnum.CHECK_STATUS_PASS.desc());
+                    break;
+                case "3" :
+                    item.setChkStatus(CheckStatusEnum.CHECK_STATUS_REFUSE.desc());
+                    break;
+                default:
+                    break;
+            }
         });
 
         String fileName = "授权取消审批报告";
