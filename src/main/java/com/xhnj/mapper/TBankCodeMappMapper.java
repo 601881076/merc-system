ee0177cc3b88b981c711ca905b145e4a6dd6ac4d
likang <1973711398@qq.com>
2021-12-06 21:23:34 +0800

    excel上传去掉循环查询

---
+++ src/main/java/com/xhnj/mapper/TBankCodeMappMapper.java
@@ -0,0 +1,14 @@
+package com.xhnj.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xhnj.model.TBankCodeMapp;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @Author: lk
+ * DateTime: 2021-12-06 20:20
+ */
+public interface TBankCodeMappMapper extends BaseMapper<TBankCodeMapp> {
+}
